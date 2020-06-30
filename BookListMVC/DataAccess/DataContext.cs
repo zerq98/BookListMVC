1efdd42741ce21ccc3f3ec3e16f4c32874041b54
Mateusz Trybuła <zerq1998@gmail.com>
2020-06-30 12:22:58 +0200

    Add project files.

---
+++ BookListMVC/DataAccess/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using BookListMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListMVC.DataAccess
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base() { }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ BookListMVC/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookListMVC.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Book title")]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Author { get; set; }
+
+        [MaxLength(20)]
+        public string ISBN { get; set; }
+    }
+}
